125dcbf1fad8585df0d918e126f684c8b1a03c55
Benis Tambe <will.jenifer@yahoo.com>
2022-04-27 18:28:56 -0400

    reverting to previous solution

--- shirt-store.js
+++ shirt-store.js
@@ -3,16 +3,10 @@ import { discounter } from "./discounter";
 export const cost = (shirts) => {
 
   const uniqueItems = new Set(shirts).size;
-  let theDiscount = discounter(uniqueItems);
-
+  let thediscount = discounter(uniqueItems);
   let fullPrice = shirts.length * 800.0
-  
 
-  console.log("$$$$$ Unique Items, The Discount, The Full Price $$$$$$")
-  console.log(uniqueItems)
-  console.log(theDiscount)
-  console.log(fullPrice)
 
-  return fullPrice - (fullPrice*theDiscount) ;
+  return fullPrice - (fullPrice*thediscount) ;
 
 };
