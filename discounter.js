330f10ecf37b265545a25e5d6bee3ee400aee76b
Benis Tambe <will.jenifer@yahoo.com>
2022-04-27 17:56:21 -0400

    discounter returns the appropriate discount value

--- discounter.js
+++ discounter.js
@@ -1,3 +1,13 @@
-export const discounter = () => {
-
+export const discounter = (shirtVariations) => {
+    //Creating Discount 
+    if(shirtVariations == 2){
+        return 0.05;
+    }else if(shirtVariations == 3){
+        return 0.10;
+    }else if(shirtVariations == 4){
+        return 0.20;
+    }else if(shirtVariations == 5){
+        return 0.25;
+    }
+    return 0;
   };
