1678f4d99c472284dff0f9bb533e5ffe98d60f60
Benis Tambe <will.jenifer@yahoo.com>
2022-04-27 20:25:55 -0400

    fixed calculation error

--- shirt-store.js
+++ shirt-store.js
@@ -1,12 +1,35 @@
 import { discounter } from "./discounter";
 
 export const cost = (shirts) => {
+  // const cartTracker = {};
 
-  const uniqueItems = new Set(shirts).size;
-  let thediscount = discounter(uniqueItems);
-  let fullPrice = shirts.length * 800.0
+  let counter = 0;
+  let grandTotal = 0;
 
+  //sort the array from asc to desc
+  shirts = shirts.sort();
+  const uniqueItemsArr = new Set(shirts);
 
-  return fullPrice - (fullPrice*thediscount) ;
+  while(JSON.stringify(uniqueItemsArr)==JSON.stringify(shirts)){
+    console.log("#####")
+    console.log(shirts)
+    for(let i = 0; i < shirts.length-1; i++){
+      if(shirts[i] !== shirts[i+1]){
+        counter += 1;
+        //remove last occurence of repeat item in array
+        shirts.splice(i, 1);
+        console.log(shirts)
+      }
+    }
 
+    grandTotal += ((counter * 800) - ((counter * 800) * discounter(counter)))
+    counter = 0
+
+  }
+
+  //remaining items in arr will not be duplicates
+  let theRemainingDiscount = ((new Set(shirts).size * 800) - ((new Set(shirts).size * 800) * discounter(shirts.length)));
+  
+
+  return theRemainingDiscount + grandTotal;
 };
---
+++ uniqueArr.js
@@ -0,0 +1,29 @@
+// import { discounter } from "./discounter";
+
+// export const uniqueArr = (shirts) => {
+//   // const cartTracker = {};
+
+//   let counter = 0;
+//   let grandTotal = 0;
+//   // let fullPrice = shirts.length * 800.0;
+
+//   shirts = shirts.sort();
+
+//   while(shirts.length !== new Set(shirts).size){
+//     console.log("#####")
+//     console.log(shirts)
+//     for(let i = 0; i < shirts.length; i++){
+//       console.log(shirts[i])
+//         if(shirts[i] !== shirts[i+1] &&  shirts[i+1] !== undefined){
+//           counter += 1;
+//           shirts.splice(i, 1);
+//           console.log(shirts)
+//         }
+//       }
+      
+//     }
+
+//   }
+
+ 
+// };
